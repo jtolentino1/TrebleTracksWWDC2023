063612c099a5a67e1a64b97a791d43244e45fb1d
Joshua Tolentino <joshuatolentino218@gmail.com>
2023-04-08 23:05:55 -0600

    init

--- ContentView.swift
+++ ContentView.swift
@@ -1,3 +1,10 @@
+//
+//  ContentView.swift
+//  TrebleTracksWWDC2023
+//
+//  Created by Joshua Tolentino on 2023-04-08.
+//
+
 import SwiftUI
 
 struct ContentView: View {
--- MyApp.swift
+++ MyApp.swift
@@ -1,3 +1,10 @@
+//
+//  MyApp.swift
+//  TrebleTracksWWDC2023
+//
+//  Created by Joshua Tolentino on 2023-04-08.
+//
+
 import SwiftUI
 
 @main
