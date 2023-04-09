51dd933d449e1efbecb58522d67d4e7defa24f48
Joshua Tolentino <joshuatolentino218@gmail.com>
2023-04-09 00:11:46 -0600

    added functionality for playing sheet music (represented as matrices at the moment)

--- SoundPlayer.swift
+++ SoundPlayer.swift
@@ -12,22 +12,42 @@ class SoundPlayer {
     
     func play(sounds: [String]) {
         for sound in sounds {
-            // Load sound file
             guard let soundURL = Bundle.main.url(forResource: sound, withExtension: "wav") else {
                 print("Sound file not found")
                 return
             }
             
             do {
-                // Create audio player and play sound
                 let audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
                 audioPlayer.play()
-                
-                // Add audio player to array
                 audioPlayers.append(audioPlayer)
             } catch {
                 print("Error playing sound: \(error.localizedDescription)")
             }
+        }
+    }
+    
+    func playMatrix(matrix: [[Int]]) {
+        let noteNames = ["C", "D", "E", "F", "G", "A", "B"]
+        let lowestNoteOctave = 4
+        let highestNoteOctave = 6
+        
+        for columnIndex in 0..<matrix[0].count {
+            var notesToPlay: [String] = []
+            for rowIndex in 0..<matrix.count {
+                if matrix[rowIndex][columnIndex] == 1 {
+                    let noteIndex = (matrix.count - 1) - rowIndex // reverse the index to start from C4
+                    let octave = lowestNoteOctave + noteIndex / noteNames.count
+                    let noteName = noteNames[noteIndex % noteNames.count]
+                    let note = "\(noteName)\(octave)"
+                    notesToPlay.append(note)
+                }
+            }
+            play(sounds: notesToPlay)
+            
+            // us = 60,000,000 / BPM
+            // so this delay right now is 300 BPM (quarter notes)
+            usleep(useconds_t(200000))
         }
     }
 }
