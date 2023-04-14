93f50aac62dc205770dfc0c67ae16bf411d31165
Joshua Tolentino <joshuatolentino218@gmail.com>
2023-04-14 00:18:18 -0600

    finished page 3

--- PageComponents.swift
+++ PageComponents.swift
@@ -281,3 +281,294 @@ struct TrebbleCleffStaffWithoutMeasuresWithBasicNotes: View{
     }
 }
 
+struct TrebbleCleffStaffWithoutMeasuresWithFullNotes: View{
+    
+    @State private var scrollerOffset: CGFloat = 0
+    @State private var scrollCounter: CGFloat = 1
+    
+    @State private var interruptFlag: Bool = false
+    @State private var isPlaying: Bool = false
+    @State private var bpm = 150
+    
+    var soundPlayer = SoundPlayer()
+    
+    func updateScroller() {
+        if(scrollCounter == 4){
+            scrollerOffset += 1
+        }
+        if(scrollCounter == 10){
+            scrollerOffset += 1
+        }
+        if(scrollCounter == 13){
+            scrollerOffset += 1
+        }
+        scrollerOffset += 56
+
+        scrollCounter += 1
+    }
+    
+    var body: some View{
+        
+        ZStack{
+            HStack(spacing: 20) {
+                Image("trebleclef")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.black)
+                    .frame(width: 175, height: 175)
+                    .padding(.leading, -80)
+                    .id("trebleclefanchor")
+                
+                ForEach(1...4, id: \.self) { measure in
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        ForEach(0..<5) { _ in
+                            Staff()
+                        }
+                        
+                        // this is here for pure formatting
+                        ZStack{
+                        }
+                        
+                    }
+                }
+                .frame(width: 160, alignment: .leading)
+                .padding(.top, 18)
+                .padding(.leading, 30)
+            }
+            .background(
+            HStack(alignment: .center){
+                Button(
+                    action: {
+                            
+                        if(!isPlaying){
+                            
+                            // initializers
+                            scrollerOffset = 0
+                            scrollCounter = 1
+                            
+                            Timer.scheduledTimer(withTimeInterval: (Double(60000000) / Double(150)) / 1000000, repeats: true) { timer in
+                                
+                                withAnimation(.easeOut(duration: (Double(60000000) / Double(bpm*8)) / 1000000)){
+                                    withAnimation(.linear(duration: 0.1)){
+                                        updateScroller()
+                                    }
+                                }
+                                
+                                if ((scrollCounter == 15) || (interruptFlag == true)){
+                                    interruptFlag = true
+                                    timer.invalidate()
+                                }
+                            }
+                            
+                            DispatchQueue.global(qos: .userInteractive).async {
+                                soundPlayer.playMatrix(matrix: MusicData.simpleScale, bpm: bpm)
+                            }
+                        }
+                            
+                        if(isPlaying){
+                            interruptFlag = true
+                            soundPlayer.stop()
+                        }
+                        
+                        isPlaying.toggle()
+                        
+                    },
+                    label: {
+                        Image(systemName: !isPlaying ? "play.circle.fill" : "pause.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.black)
+                            .opacity(interruptFlag ? 0.25 : 1)
+                    }
+                )
+                .disabled(interruptFlag)
+                
+                Button(
+                    action: {
+                        isPlaying = false
+                        interruptFlag = false
+                        scrollerOffset = 0
+                        scrollCounter = 1
+                    },
+                    label: {
+                        Image(systemName: "backward.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.black)
+                            .opacity(!interruptFlag ? 0.25 : 1)
+                    })
+                .disabled(!interruptFlag)
+                .padding(.trailing, 20)
+            }
+                .padding(.leading, 20)
+                .padding(.top, 320)
+            
+            )
+            .background(
+                GeometryReader { geo in
+                    HStack{
+                        Group{
+                            QuarterNote()
+                                .background(
+                                    Path { path in
+                                        path.move(to: CGPoint(x: -10, y: 0))
+                                        path.addLine(to: CGPoint(x:30, y: 0))
+                                    }
+                                        .stroke(Color.black, lineWidth: 1.5)
+                                        .frame(height: 2)
+                                )
+                                .background(
+                                    Text("C")
+                                        .position(x:10,y:50)
+                                )
+                                .position(x:0,y:150)
+                            
+                            QuarterNote()
+                            
+                                .background(
+                                    Text("D")
+                                        .position(x:10,y:61)
+                                )
+                                .position(x: 0,y:139)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("E")
+                                        .position(x:10,y:71)
+                                )
+                                .position(x: 0,y:129)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("F")
+                                        .position(x:10,y:82)
+                                )
+                                .position(x: 0,y:118)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("G")
+                                        .position(x:10,y:92)
+                                )
+                                .position(x: 0,y:108)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("A")
+                                        .position(x:10,y:104)
+                                )
+                                .position(x: 0,y:96)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("B")
+                                        .position(x:10,y:114)
+                                )
+                                .position(x: 0,y:86)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("C")
+                                        .position(x:10,y:126)
+                                )
+                                .position(x: 0,y:74)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("D")
+                                        .position(x:10,y:137)
+                                )
+                                .position(x: 0,y:63)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("E")
+                                        .position(x:10,y:148)
+                                )
+                                .position(x: 0,y:52)
+                            
+                        }
+                        Group{
+                            QuarterNote()
+                                .background(
+                                    Text("F")
+                                        .position(x:10,y:159)
+                                )
+                                .position(x: 0,y:41)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("G")
+                                        .position(x:10,y:170)
+                                )
+                                .position(x: 0,y:30)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("A")
+                                        .position(x:10,y:181)
+                                )
+                                .background(
+                                    Path { path in
+                                        path.move(to: CGPoint(x: -10, y: 0))
+                                        path.addLine(to: CGPoint(x:30, y: 0))
+                                    }
+                                        .stroke(Color.black, lineWidth: 1.5)
+                                        .frame(height: 2)
+                                )
+                                .position(x: 0,y:19)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("B")
+                                        .position(x:10,y:193)
+                                )
+                                .background(
+                                    Path { path in
+                                        path.move(to: CGPoint(x: -10, y: 11))
+                                        path.addLine(to: CGPoint(x:30, y: 11))
+                                    }
+                                        .stroke(Color.black, lineWidth: 1.5)
+                                        .frame(height: 2)
+                                )
+                                .position(x: 0,y:7)
+                            
+                            QuarterNote()
+                                .background(
+                                    Text("C")
+                                        .position(x:10,y:203)
+                                )
+                                .background(
+                                    Path { path in
+                                        path.move(to: CGPoint(x: -10, y: 0))
+                                        path.addLine(to: CGPoint(x:30, y: 0))
+                                    }
+                                        .stroke(Color.black, lineWidth: 1.5)
+                                        .frame(height: 2)
+                                )
+                                .position(x: 0,y:-3)
+                        }
+                    }
+                    .padding(.leading, 100)
+                    
+                }
+            )
+            .background(
+                GeometryReader { geo in
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .opacity(0.15)
+                        .frame(width: 20, height: 190)
+                        .offset(
+                            x: CGFloat(scrollerOffset + 90),
+                            y: -20)
+                }
+            )
+            .frame(height: 280)
+        }
+    }
+}
+
--- PageView.swift
+++ PageView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PageView: View {
     // Track the current page using a state variable
-    @State private var currentPage = 3
+    @State private var currentPage = 4
     @Namespace var namespace
     
     var body: some View {
@@ -23,13 +23,14 @@ struct PageView: View {
                             Text("Learning to read sheet music unlocks a whole new world of musical possibilities! It's like a secret code that tells you which notes to play, how long to hold them, and even how loud or soft to play them.")
                             Text("Once you master this language, you can play any song you want and even create your own music. It's like having a superpower that lets you express yourself through music.")
                         }
-                        .matchedGeometryEffect(id: "topTextP1", in: namespace)
                         .frame(width: 500)
+                        .matchedGeometryEffect(id: "topTextP1", in: namespace)
+                        .matchedGeometryEffect(id: "musicalStaffP1", in: namespace)
                         
                         Text("Ready to get started?")
                             .padding(.vertical, 10)
-                            .matchedGeometryEffect(id: "bottomText", in: namespace)
                             .frame(width: 500)
+                            .matchedGeometryEffect(id: "bottomText", in: namespace)
                     
                         
                         Button("Get Started") {
@@ -63,6 +64,7 @@ struct PageView: View {
                         
                     TrebbleCleffStaffWithoutMeasures()
                         .matchedGeometryEffect(id: "musicStaff", in: namespace)
+                        .matchedGeometryEffect(id: "musicalStaffP1", in: namespace)
                     
                     
                     VStack(alignment: .leading, spacing: 15){
@@ -87,6 +89,8 @@ struct PageView: View {
                                     .foregroundColor(.black)
                             })
                         .matchedGeometryEffect(id: "prev", in: namespace)
+                        
+                        Text("\(currentPage-1)")
 
                         Button(
                             action: {
@@ -114,23 +118,26 @@ struct PageView: View {
                         VStack(alignment: .leading, spacing: 15){
                             Text("I went ahead and placed down some notes for you. I also labeled them so you can better understand!")
                             
-                            Text("As you can see, the notes either go on a space or a line on the musical staff.")
+                            Text("As you can see, the notes either go on a space or a line on the musical staff. Also, each of the notes present below are called **quarter notes**, we will talk more above this later.")
                             
                             
                         }
                         .frame(width: 500)
                         .matchedGeometryEffect(id: "topTextP2", in: namespace)
+                        .matchedGeometryEffect(id: "topTextP3", in: namespace)
+
                     
                         TrebbleCleffStaffWithoutMeasuresWithBasicNotes()
                             .matchedGeometryEffect(id: "musicStaff", in: namespace)
-                    
+                            .matchedGeometryEffect(id: "musicStaff2", in: namespace)
+
                         Text("However, as stated before, we are in the **treble clef**, these are not all the notes present in this musical staff!")
                             .padding(.top, 70)
                             .padding(.bottom, 10)
-                            .matchedGeometryEffect(id: "bottomText", in: namespace)
                             .frame(width: 500)
+                            .matchedGeometryEffect(id: "bottomText", in: namespace)
+                            .matchedGeometryEffect(id: "bottomText2", in: namespace)
 
-                        
                     HStack(alignment: .center, spacing: 100){
                         Button(
                             action: {
@@ -145,6 +152,8 @@ struct PageView: View {
                                     .frame(width: 30, height: 30)
                                     .foregroundColor(.black)
                             })
+                        
+                        Text("\(currentPage-1)")
 
                         Button(
                             action: {
@@ -167,7 +176,61 @@ struct PageView: View {
                     
                 // ========== PAGE 4 ============= //
                 } else if currentPage == 4 {
-                    Text("Hello World!")
+                    VStack(alignment: .leading, spacing: 15){
+                        Text("Here are the rest of the notes in the treble clef. As you can see, it is a wide range of notes. Since the letters are repeating, we typically number each note to uniquely identify them. ")
+                        
+                        Text("For example, the leftmost C in this musical staff is known as **C4** or **middle C**. The numbering increments when another C is reached. So the next C is labled **C5** and the final C is labled **C6** and each note in between follows that number D4, E4, etc.")
+                        
+                        
+                    }
+                    .frame(width: 500)
+                    .matchedGeometryEffect(id: "topTextP3", in: namespace)
+
+                    TrebbleCleffStaffWithoutMeasuresWithFullNotes()
+                        .matchedGeometryEffect(id: "musicStaff2", in: namespace)
+                
+                    Text("Feel free to press the play button above the hear the notes! 🎶")
+                        .padding(.top, 70)
+                        .padding(.bottom, 10)
+                        .frame(width: 500)
+                        .matchedGeometryEffect(id: "bottomText2", in: namespace)
+
+                HStack(alignment: .center, spacing: 100){
+                    Button(
+                        action: {
+                            withAnimation(.easeInOut(duration: 0.1)){
+                                self.currentPage = 3
+                            }
+                        },
+                        label:{
+                            Image(systemName: "arrowshape.left.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                        })
+                    
+                    Text("\(currentPage-1)")
+
+                    Button(
+                        action: {
+                            withAnimation(.easeInOut(duration: 0.1)){
+                                self.currentPage = 5
+                            }
+                        },
+                        label:{
+                            Image(systemName: "arrowshape.right.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                        })
+
+
+                    }
+                .padding(.top, 30)
+                .matchedGeometryEffect(id: "navButton", in: namespace)
+                
                 }
                 
             }
